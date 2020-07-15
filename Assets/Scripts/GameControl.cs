3446c69214f3b5479d56d365cdf09d11000a1f27
Ruslan Melnyk <rusaqwerty535@gmail.com>
2020-07-15 16:45:16 +0300

    v0.1

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour
+{
+    int score;
+    public GameObject[] upSpikes;
+    public GameObject[] downSpikes;
+    public GameObject player;
+    public Player playerScript;
+    int numOfSpikes;
+    Text scoreText;
+    
+
+    private void Start()
+    {
+        scoreText = GameObject.Find("Score").GetComponent<Text>();
+        playerScript = player.GetComponent<Player>();
+        score = 0;
+        numOfSpikes = 3;
+        foreach (var spike in downSpikes)
+        {
+            spike.SetActive(false);
+        }
+        foreach (var spike in upSpikes)
+        {
+            spike.SetActive(false);
+        }
+    }
+
+    public IEnumerator Restart()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void ChangeSpikes(bool up)
+    {
+        List<int> s =  RandomList(numOfSpikes, new List<int>() {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, 0);
+        if (up)
+        {
+            
+            for (int i = 0; i < s.Count; i++)
+            {
+                if (s[i] == 1) downSpikes[i].SetActive(true);
+            }
+            foreach (var spike in upSpikes)
+            {
+                spike.SetActive(false);
+            }
+        }
+        else
+        {
+            foreach (var spike in downSpikes)
+            {
+                spike.SetActive(false);
+            }
+            for (int i = 0; i < s.Count; i++)
+            {
+                if (s[i] == 1) upSpikes[i].SetActive(true);
+            }
+        }
+    }
+
+    List<int> RandomList(int limit, List<int> mass, int c)
+    {
+        int count = c;
+        List<int> list = mass;
+        for(int i = 0; i < list.Count; i++)
+        {
+            if (Random.Range(1, 5) == 2 && count < limit)
+            {
+                list[i] = 1;
+                ++count;
+            }
+        }
+        if (count < limit)
+            list = RandomList(limit, list, count);
+        return list;
+    }
+
+    public void AddScore()
+    {
+        score += 1;
+        scoreText.text = score.ToString();
+        if (score % 3 == 0 && numOfSpikes < 17) numOfSpikes += 1;
+        if (Input.GetKeyDown(KeyCode.Escape)) Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+    int force;
+    float move;
+    bool up;
+    GameControl GC;
+    [SerializeField]
+    private GameObject bum;
+
+    private void Start()
+    {
+        up = true;
+        force = 400;
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        rb.AddForce(new Vector2(0, force));
+        GC = GameObject.Find("UpSpikes").GetComponent<GameControl>();
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall")) Move();
+        if (collision.gameObject.CompareTag("Spike")) Death();
+    }
+    
+    void Move()
+    {
+        force += 10;
+        GC.AddScore();
+        GC.ChangeSpikes(up);
+        if (up)
+        {
+            rb.AddForce(new Vector2(0, -force));
+            up = false;
+        }
+        else
+        {
+            rb.AddForce(new Vector2(0, force));
+            up = true;
+        }
+    }
+
+    void Death()
+    {
+        Destroy(gameObject);
+        GC.StartCoroutine("Restart");
+        Instantiate(bum, transform.position, Quaternion.identity);
+    }
+
+    public bool GetMove()
+    {
+        return up;
+    }
+
+    public void MoveRight()
+    {
+        Debug.Log("Gi");
+        transform.position += new Vector3(0.1f, 0);
+    }
+    
+    private void FixedUpdate()
+    {
+        move = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(0.1f * move, 0);
+    }
+}
